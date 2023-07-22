af619a6c1576c86f0f960ef1c1d9fbb8084b7c93
yejet <saeta11@gmail.com>
2023-07-22 18:44:38 -0400

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -9,6 +9,7 @@ from flask import Flask
 from service import config
 from service.common import log_handlers
 from flask_talisman import Talisman
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
@@ -18,6 +19,7 @@ csp = {
     'object-src': '\'none\''
 }
 talisman = Talisman(app, content_security_policy=csp)
+CORS(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
